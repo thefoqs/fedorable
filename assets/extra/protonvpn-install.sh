bb0572f2a2cbb413f171191aeb15bbda3cdbe8b5
Gabriel Maia <gabrielmaia720@protonmail.com>
2025-05-23 20:16:16 -0300

    Added proton vpn gtk App install support.

---
+++ assets/extra/protonvpn-install.sh
@@ -0,0 +1,23 @@
+# Ensure the script is run as root
+if [[ $EUID -ne 0 ]]; then
+   echo "This script must be run as root (use sudo)"
+   exit 1
+fi
+
+echo "Installing Proton VPN GTK App"
+
+# Step 1: Download Proton VPN rpm package
+cd ~/Downloads
+wget "https://repo.protonvpn.com/fedora-$(cat /etc/fedora-release | cut -d' ' -f 3)-stable/protonvpn-stable-release/protonvpn-stable-release-1.0.3-1.noarch.rpm"
+
+# Step 2: Enable Proton VPN repository
+sudo dnf install -y ./protonvpn-stable-release-1.0.3-1.noarch.rpm && sudo dnf check-update --refresh "" --unattended
+
+# Step 3: Install proton-vpn-gtk-app
+sudo dnf install -y proton-vpn-gnome-desktop "" --unattended
+
+# Step 4: Enable Gnome tray icon support
+sudo dnf install libappindicator-gtk3 gnome-shell-extension-appindicator gnome-extensions-app # Add Gnome tray support
+
+# Step 5: Remove downloaded rpm package
+rm ./protonvpn-stable-release-1.0.3-1.noarch.rpm

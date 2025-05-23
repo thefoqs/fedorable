e45b4817576fc9b598c55b03b2432db94bedf5fc
Gabriel Maia <gabrielmaia720@protonmail.com>
2025-05-23 19:30:37 -0300

    "install_extras" function adjust: Removed sound-and-video update since it doesn't exist anymore. Removed libdvdcss cause i don't need it. Remove gstreamer-ffmpeg since it doesn't exist in repository. Removed group Multimedia related upgrade since because doesn't exist anymore.Removed font iosevka copr repo. Added "cabextract" and "xorg-x11-font-utils" to satisfy microsoft fonts dependencies.

--- fedorable.sh
+++ fedorable.sh
@@ -126,17 +126,12 @@ install_oh_my_zsh() {
 # Function to install extras
 install_extras() {
     echo "Installing Extras"
-    sudo dnf groupupdate -y sound-and-video
     sudo dnf swap -y ffmpeg-free ffmpeg --allowerasing
-    sudo dnf install -y libdvdcss
-    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg gstreamer-ffmpeg
+    sudo dnf install -y gstreamer1-plugins-{bad-\*,good-\*,ugly-\*,base} gstreamer1-libav --exclude=gstreamer1-plugins-bad-free-devel ffmpeg
     sudo dnf install -y lame\* --exclude=lame-devel
-    sudo dnf group upgrade -y --with-optional Multimedia
-    sudo dnf config-manager --set-enabled fedora-cisco-openh264
     sudo dnf install -y gstreamer1-plugin-openh264 mozilla-openh264
-    sudo dnf copr enable peterwu/iosevka -y
+    sudo dnf install -y cabextract xorg-x11-font-utils
     sudo dnf update -y
-    sudo dnf install -y iosevka-term-fonts jetbrains-mono-fonts-all terminus-fonts terminus-fonts-console google-noto-fonts-common fira-code-fonts cabextract xorg-x11-font-utils fontconfig
     sudo rpm -i https://downloads.sourceforge.net/project/mscorefonts2/rpms/msttcore-fonts-installer-2.6-1.noarch.rpm
     notify "All done"
 }
